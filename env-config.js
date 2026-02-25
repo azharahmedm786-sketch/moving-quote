83c750875e23d1c9bb2ecb199335222dc950ed33
azharahmedm786-sketch <azharahmedm786@gmail.com>
2026-02-25 13:40:30 +0530

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -1,3 +1,6 @@
 window.ENV = {
-  GOOGLE_MAPS_KEY: "AIzaSyA0_uDJsx8EqoChj865kWk_vveoPfehdzQ"
+  GOOGLE_MAPS_KEY: "AIzaSyA0_uDJsx8EqoChj865kWk_vveoPfehdzQ",
+    
+    // Firebase Auth key - NO referrer restrictions
+    FIREBASE_AUTH_KEY: 'AIzaSyCNZdOeUlwlI9kwkzD05R1xEy8EyuP8VJo'
 };
