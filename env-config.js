535e456cc2e4e9d9d56674fd1cb1cc3970e0df51
azharahmedm786-sketch <azharahmedm786@gmail.com>
2026-03-14 22:31:04 +0530

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -1,8 +1,9 @@
 window.ENV = {
   GOOGLE_MAPS_KEY: "AIzaSyA0_uDJsx8EqoChj865kWk_vveoPfehdzQ",
-    
-    // Firebase Auth key - NO referrer restrictions
-    FIREBASE_AUTH_KEY: 'AIzaSyCNZdOeUlwlI9kwkzD05R1xEy8EyuP8VJo'
-  // env-config.js
+
+  // Firebase Auth key
+  FIREBASE_AUTH_KEY: "AIzaSyCNZdOeUlwlI9kwkzD05R1xEy8EyuP8VJo",
+
+  // Razorpay Public Key
   RAZORPAY_KEY: "rzp_live_SR6N14UYbYB42e"
 };
